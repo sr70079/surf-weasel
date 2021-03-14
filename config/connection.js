1ddb00225e268c1784b5452686112930a8c4d64e
mdu830 <mdu830@yahoo.com>
2021-03-14 18:53:26 -0400

    minor editing and testing for sequelize

--- config/connection.js
+++ config/connection.js
@@ -1,35 +1,24 @@
-const Sequelize = require('sequelize');
 
-// database name, username , password, email, name, {connection info}
-const sequelize = new Sequelize('users_db', 'admin', '12345678', 'joeshmo@gmail.com', 'Joe', {
-  dialect: 'mysql',
-  host: 'localhost',
-  port: 3306
-
-});
-
-module.exports = sequelize;
-
-// module.exports = {
-//   development: {
-//     username: process.env.SEQUELIZE_USER,
-//     password: process.env.SEQUELIZE_PASSWORD,
-//     database: 'userinfo_db',
-//     dialect: 'mysql',
-//     host: process.env.SEQUELIZE_HOST,
-//     port: 3306
-//   },
-//   test: {
-//     username: process.env.TU,
-//     password: process.env.TP || null,
-//     database: 'userinfo_test',
-//     host: 'localhost',
-//     port: 3306,
-//     dialect: 'mysql',
-//     logging: false
-//   },
-//   production: {
-//     'use_env_variable': 'JAWSDB_URL',
-//     dialect: 'mysql'
-//   }
-// };
+module.exports = {
+  development: {
+    username: process.env.SEQUELIZE_USER,
+    password: process.env.SEQUELIZE_PASSWORD,
+    database: 'userinfo_db',
+    dialect: 'mysql',
+    host: process.env.SEQUELIZE_HOST,
+    port: 3306
+  },
+  test: {
+    username: process.env.TU,
+    password: process.env.TP || null,
+    database: 'userinfo_test',
+    host: 'localhost',
+    port: 3306,
+    dialect: 'mysql',
+    logging: false
+  },
+  production: {
+    'use_env_variable': 'JAWSDB_URL',
+    dialect: 'mysql'
+  }
+};
