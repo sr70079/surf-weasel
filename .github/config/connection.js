c3bbf208ef93f98fa3aa6fd36d7342ef49835173
Matthew Eggleston <MW_Eggleston@msn.com>
2021-03-18 17:24:42 -0400

    Rewrote the file structure to make functional.

---
+++ .github/config/connection.js
@@ -0,0 +1,26 @@
+
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: process.env.SEQUELIZE_USER,
+    password: process.env.SEQUELIZE_PASSWORD,
+    database: 'userinfo_db',
+    dialect: 'mysql',
+    host: process.env.SEQUELIZE_HOST,
+    port: 3306
+  },
+  test: {
+    username: process.env.TU,
+    password: process.env.TP || null,
+    database: 'userinfo_test',
+    host: 'localhost',
+    port: 3306,
+    dialect: 'mysql',
+    logging: false
+  },
+  production: {
+    'use_env_variable': 'JAWSDB_URL',
+    dialect: 'mysql'
+  }
+};
