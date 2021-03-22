bbf7d071e465f20c022e86fe787e69ab8c48f6d4
mdu830 <mdu830@yahoo.com>
2021-03-21 22:41:24 -0400

    added signup password check and error message if user already exists

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -4,7 +4,14 @@ console.log('app.js is running on this');
 $('#signupBtn').on('click', function (event) {
   event.preventDefault();
 
-  // need checking if the two passwords inputed match
+  // check if passwords match
+  const passwordTxt = $('#inputPass1').val().trim();
+  const passwordCheck = $('#inputPass2').val().trim();
+
+  if (passwordTxt !== passwordCheck) {
+    $('#CheckPasswordMatch').html('Passwords do not not match!');
+    return;
+  };
 
   const firstName = $('#inputFirst').val().trim();
   const lastName = $('#inputLast').val().trim();
@@ -12,16 +19,21 @@ $('#signupBtn').on('click', function (event) {
   const newAccount = {
     name: firstName + ' ' + lastName,
     email: $('#inputEmail').val().trim(),
-    password: $('#inputPass2').val().trim()
+    password: $('#inputPass2').val().trim(),
+    fav_beach: null
   };
-
-  console.log(newAccount);
 
   if (newAccount.password.length > 0 && newAccount.email.length > 0 && newAccount.password.length > 0 && newAccount.name.length > 0) {
     $.ajax({
       type: 'POST',
       url: '/api/register',
-      data: newAccount
+      data: newAccount,
+      statusCode: {
+        // if user already exists in database show this message
+        403: function () {
+          $('#CheckPasswordMatch').html('That user already exists in our database');
+        }
+      }
     }).then(() => {
       // window.location.href = '/';
       console.log(newAccount);
