275aba872249add9fc2dd270e8a7c8a48dc91dd7
mdu830 <mdu830@yahoo.com>
2021-04-04 12:30:38 -0400

    added some basic popmotion animations

--- public/assets/js/popmotion.js
+++ public/assets/js/popmotion.js
@@ -1,1 +1,43 @@
-// console.log('i am popmotion');
+/* eslint-disable no-undef */
+const loginCard = popmotion.styler(document.querySelector('.card'));
+// login card animate
+popmotion.tween({
+  from: {
+    scale: 0.7,
+    opacity: 0
+  },
+  to: {
+    scale: 1,
+    opacity: 1
+  },
+  duration: 1000
+}).start(loginCard.set);
+
+const signupCard = popmotion.styler(document.querySelector('.signupCard'));
+// signup card animate
+popmotion.tween({
+  from: {
+    scale: 0.7,
+    opacity: 0
+  },
+  to: {
+    scale: 1,
+    opacity: 1
+  },
+  duration: 1000
+}).start(signupCard.set);
+
+// const dashboard = popmotion.styler(document.querySelector('#main-constainer'));
+// const dashContainer = document.querySelector('main-container');
+// // signup card animate
+// popmotion.tween({
+//   from: {
+//     scale: 0.7,
+//     opacity: 0
+//   },
+//   to: {
+//     scale: 1,
+//     opacity: 1
+//   },
+//   duration: 1000
+// }).start(dashboard.set);
